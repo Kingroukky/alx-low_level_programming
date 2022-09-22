87db45f175b10738464742df4ef7b1410726dd44
Kingroukky <111063615+Kingroukky@users.noreply.github.com>
2022-09-22 21:25:02 +0100

    Create main.h

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+int _putchar(char c);
+void reverse_array(int *a, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncat(char *dest, char *src, int n);
+char *string_toupper(char *a);
+char *cap_string(char *a);
+char *leet(char *a);
+void print_number(int n);
